ed33059741aa0a44c3850d479e8b23b52da1a2f1
fasyaaa <muhammad.a.f.2210@gmail.com>
2025-01-17 17:31:03 +0700

    FIX - login dosen

--- index.js
+++ index.js
@@ -1,49 +1,43 @@
 const express = require("express");
-const session = require("express-session");
 const app = express();
 const port = 3000;
+const session = require("express-session");
+
+// Konfigurasi session
+app.use(
+  session({
+    secret: "your-secret-key",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }, // Gunakan true jika menggunakan HTTPS di produksi
+  })
+);
+
+// Middleware untuk menangani body-parser (untuk menangani form-data)
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 const cors = require("cors");
 app.use(cors());
-
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-app.use(
-    session({
-      secret: "your-secret-key", // Use a strong secret key
-      resave: false,
-      saveUninitialized: true,
-      cookie: { secure: false }, // Set to true if using HTTPS
-    })
-  );
 
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
 app.use(express.static("public"));
 
-// import routes file
-
-// Login
+// Import routes
 const loginRouter = require("./routes/login");
-app.use("/portalnilai", loginRouter);
+const mahasiswaRouter = require("./routes/mahasiswa");
+const dosenRouter = require("./routes/dosen");
 
-// redirect awalan
+// Routing
+app.use("/portalnilai", loginRouter);
 app.get("/", (req, res) => {
   res.redirect("/portalnilai/");
 });
-
-const mahasiswaRouter = require("./routes/mahasiswa");
 app.use("/portalnilai/mahasiswa", mahasiswaRouter);
-
-const dosenRouter = require("./routes/dosen");
 app.use("/portalnilai/dosen", dosenRouter);
 
-const nilaiRouter = require("./routes/nilai");
-app.use("/portalnilai/dosen/nilai", nilaiRouter);
-
 app.listen(port, () => {
-    console.log(`Website listening at http://localhost:${port}`);
-  });
+  console.log(`Website listening at http://localhost:${port}`);
+});
